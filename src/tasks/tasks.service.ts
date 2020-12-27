7feb6e973009d300b90c1ea18656e5f8817276e0
t4h4 <thyasinerkan@gmail.com>
2020-12-27 21:03:43 +0300

    creating a task v-2

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, ParseUUIDPipe } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid';
+import { v1 as uuidv1 } from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -12,7 +12,7 @@ export class TasksService {
     
     createTask(title:string, description:string): Task {
         const task: Task = {
-            id: uuid(),
+            id: uuidv1(),
             title,
             description,
             status: TaskStatus.OPEN,
